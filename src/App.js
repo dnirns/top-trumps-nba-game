937f5b7c2b37b665e59762fc5ccb5d81a33cd549
dnirns <daniel.irons@gmail.com>
2021-02-01 11:04:18 +0000

    basic tailwind styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import GameNba from '../src/components/GameNba'
 
-import '../src/styles/styles.scss'
+import '../src/styles/styles.css'
 
 export default function App() {
   return (
